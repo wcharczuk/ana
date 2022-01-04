214f2011c673613030aadeb6addc5d3331fb13a4
Will Charczuk <wcharczuk@users.noreply.github.com>
2022-01-04 12:17:24 -0800

    UGLY

--- main.go
+++ main.go
@@ -42,9 +42,8 @@ func main() {
 	if *flagVerbose {
 		fmt.Println("Permutations:")
 		for p := range inputPermutations {
-			fmt.Println(p)
+			fmt.Println("\t" + p)
 		}
-
 		fmt.Println("Found:")
 	}
 
@@ -57,38 +56,92 @@ func main() {
 	for dictScanner.Scan() {
 		dictWord = dictScanner.Text()
 		if inputPermutations.Has(dictWord) {
-			fmt.Println(dictWord)
+			if *flagVerbose {
+				fmt.Println("\t" + dictWord)
+			} else {
+				fmt.Println(dictWord)
+			}
 		}
 	}
 }
 
-type Set[A comparable] map[A]struct{}
+type Set[A comparable] map[A]int
 
-func (s Set[A]) Add(v A)           { s[v] = struct{}{} }
+func (s Set[A]) Add(v A)           { s[v] = s[v] + 1 }
 func (s Set[A]) Has(v A) (ok bool) { _, ok = s[v]; return }
-
-func permutations(input string) Set[string] {
-	output := make(Set[string])
-
-	inputRunes := []rune(input)
-
-	for index, c := range inputRunes {
-		_permutations(inputRunes, index, []rune{c}, output)
+func (s Set[A]) Remove(v A) {
+	count, ok := s[v]
+	if !ok {
+		return
 	}
-
+	if count == 1 {
+		delete(s, v)
+		return
+	}
+	s[v] = count - 1
+}
+func (s Set[A]) Pop() (v A) {
+	for v = range s {
+		break
+	}
+	s.Remove(v)
+	return
+}
+func (s Set[A]) Copy() Set[A] {
+	output := make(Set[A])
+	for key, count := range s {
+		output[key] = count
+	}
 	return output
 }
 
-func _permutations(inputRunes []rune, index int, working []rune, output Set[string]) {
-	if index == len(inputRunes)-1 {
-		if len(working) == len(inputRunes) {
+func (s Set[A]) Count() (output int) {
+	for _, c := range s {
+		output = output + c
+	}
+	return
+}
+
+// a,b,c,d
+
+// a
+// b,a | a,b
+// c,b,a | a,b,c
+// d,c,b,a | a,b,c,d
+// b
+// a,b | b,a
+// c,a,b |
+
+func permutations(input string) Set[string] {
+	output := make(Set[string])
+	seen := make(Set[string])
+	remaining := make(Set[rune])
+
+	for _, r := range input {
+		remaining.Add(r)
+	}
+	_permutations(remaining.Count(), nil, remaining, seen, output)
+	return output
+}
+
+func _permutations(totalLen int, working []rune, remaining Set[rune], seen, output Set[string]) {
+	if len(remaining) == 0 {
+		if len(working) == totalLen {
 			output.Add(string(working))
 		}
 		return
 	}
 
-	for subIndex := index + 1; subIndex < len(inputRunes); subIndex++ {
-		_permutations(inputRunes, subIndex, append(working, inputRunes[subIndex]), output)
-		_permutations(inputRunes, subIndex, append([]rune{inputRunes[subIndex]}, working...), output)
+	subRemaining := remaining.Copy()
+	c := subRemaining.Pop()
+	before := append([]rune{c}, working...)
+	if !seen.Has(string(before)) {
+		seen.Add(string(before))
+		_permutations(totalLen, before, subRemaining, seen, output)
+	}
+	after := append(working, c)
+	if !seen.Has(string(after)) {
+		seen.Add(string(after))
+		_permutations(totalLen, after, subRemaining, seen, output)
 	}
 }
